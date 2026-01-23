16bed4ad40174c034b4a637e54b37ea5357ddc47
carlosHrnndz <122090011+carlosHrnndz@users.noreply.github.com>
2026-01-23 13:28:23 +0100

    V6.2: Force PWA Cache Update (Hotfix for Home Button)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'microquiz-v6';
+const CACHE_NAME = 'microquiz-v6.2-hotfix';
 const ASSETS = [
     './',
     './index.html',
@@ -11,9 +11,14 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+    self.skipWaiting();
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
